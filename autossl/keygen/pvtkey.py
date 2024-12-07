0a64160fcec021bf14de3fd5eb5ee982be93b719
Chris Torok <torok.chris@gmail.com>
2024-12-07 15:37:54 -0500

    rsa key is set to a default format, but can express itself in either pkcs1 or pkcs8. Outputs are in bytes because platforms that ask for private keys often want them that way; The __str__ representation returns the format that was set upon initialization; tests for configuration and formatting are passingclear

--- autossl/keygen/pvtkey.py
+++ autossl/keygen/pvtkey.py
@@ -1,10 +1,12 @@
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization
 
 
 KEY_SIZES = { 2048, 3072, 4096 }
 PUBLIC_EXPONENTS = { 65537 }
 BACKENDS = { default_backend }
+KEY_FORMATS = {'pkcs1', 'pkcs8'}
 
 
 class RSAPrivateKey(object):
@@ -13,12 +15,20 @@ class RSAPrivateKey(object):
     PUBLIC_EXPONENT = 65537
     BACKEND = default_backend
 
-    def __init__(self, exponent: int = PUBLIC_EXPONENT, key_length: int = KEY_SIZE, backend = BACKEND):
+    FORMATS = {'pkcs1': serialization.PrivateFormat.TraditionalOpenSSL,
+               'pkcs8': serialization.PrivateFormat.PKCS8}
 
-        if not ((exponent in PUBLIC_EXPONENTS) and (key_length in KEY_SIZES) and (backend in BACKENDS)):
+    ENCODINGS = {'pem': serialization.Encoding.PEM,
+                 'der': serialization.Encoding.DER}
+
+    def __init__(
+            self, exponent: int = PUBLIC_EXPONENT, key_length: int = KEY_SIZE, backend = BACKEND, fmt: str = 'pkcs1'
+    ):
+
+        if not ((exponent in PUBLIC_EXPONENTS) and (key_length in KEY_SIZES) and (backend in BACKENDS) and fmt in KEY_FORMATS):
             raise ValueError(
                 f"RSAPrivateKey configuration not accepted. "
-                f"exponent:{exponent},key_size:{key_length},backend:{backend.__name__}"
+                f"exponent:{exponent},key size:{key_length},fmt:{fmt}"
             )
 
         self._native_key_object = rsa.generate_private_key(
@@ -27,18 +37,24 @@ class RSAPrivateKey(object):
         self.pub_exponent = exponent
         self.key_len = key_length
         self.backend_name = backend.__name__
-        pass
+        self._selected_format = fmt
+        self._selected_encoding = 'pem'
 
     def __repr__(self):
-        return f"<RSAPvtKey:{id(self)} exp:{self._pub_exponent},{self._key_len}-bits,{self._backend_name}>"
+        return f"<RSAPvtKey:{id(self)} exp:{self.pub_exponent},{self.key_len}-bits,{self._selected_format}>"
 
     def __str__(self):
-        pass
-
+        key: bytes = getattr(self, self._selected_format)
+        return key.decode(encoding='utf-8')
 
     @property
     def pkcs1(self):
-        return self._key
+        key_bytes = self._native_key_object.private_bytes(
+            self.ENCODINGS[self._selected_encoding],
+            self.FORMATS['pkcs1'],
+            serialization.NoEncryption()
+        )
+        return key_bytes
 
     @pkcs1.setter
     def pkcs1(self, _):
@@ -46,10 +62,13 @@ class RSAPrivateKey(object):
 
     @property
     def pkcs8(self):
-        return self._key
+        key_bytes = self._native_key_object.private_bytes(
+            self.ENCODINGS[self._selected_encoding],
+            self.FORMATS['pkcs8'],
+            serialization.NoEncryption()
+        )
+        return key_bytes
 
     @pkcs8.setter
     def pkcs8(self, _):
         raise ValueError("The private key cannot be set using the pkcs8 property.")
-
-    pass
