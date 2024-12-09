d8dcad0d20a3df4c8d6b03bdb48c9c1b30782f62
Chris Torok <torok.chris@gmail.com>
2024-12-08 19:45:05 -0500

    automatically use readme as pypi doc

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 from setuptools import setup, find_packages
 from autossl import version
 
+with open('./README.md', "r") as f:
+    long_description = f.read()
+
 setup(
     name='autocert',
     version=version,
     packages=find_packages(),
     install_requires=[
         'cryptography',
-    ]
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="thetisrock",
+    description="Automate your SSL certificate workflows."
 )
