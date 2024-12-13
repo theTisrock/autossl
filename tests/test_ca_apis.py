98e443700d0bfc24c77359c3bb72a507e19d6d39
Chris Torok <torok.chris@gmail.com>
2024-12-13 12:06:09 -0500

    setup testing structure

---
+++ tests/test_ca_apis.py
@@ -0,0 +1,14 @@
+import pytest
+from autossl.ca_api import DigicertCertificates
+
+
+class TestDigicertCertificatesClient(object):
+
+    def test_submit_csr(self):
+        pass
+
+    def test_check_certificate_issuance_status(self):
+        pass
+
+    def test_certificate_download(self):
+        pass
