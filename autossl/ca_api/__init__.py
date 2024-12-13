536d26173ba81797a7d27d39bd16d12b580beebe
Chris Torok <torok.chris@gmail.com>
2024-12-13 11:59:54 -0500

    test CA api collection import into python environment

---
+++ autossl/ca_api/__init__.py
@@ -0,0 +1,1 @@
+from .digicert import DigicertCertificates
---
+++ autossl/ca_api/digicert.py
@@ -0,0 +1,12 @@
+
+
+class DigicertCertificates(object):
+    """A client that interacts with Digicert to
+    1) request a certificate
+    2) check the status of a certificate
+    3) download the certificate
+
+    Not to be used for any other purpose."""
+
+    def hello(self):
+        print("hello")
